7d96f2ffb9bc8e9e865f954387b445c09f1605f2
gkreddy452 <kasturi.gopal@mac-14-15-142.local>
2017-09-21 17:56:07 +0530

    - Initial committed...

---
+++ MyDemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDemoProject
+//
+//  Created by kasturi gopal on 21/09/17.
+//  Copyright © 2017 kasturi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
